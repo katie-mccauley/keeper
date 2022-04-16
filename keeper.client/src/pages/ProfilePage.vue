a8f0fd14dadf4c2ad5f907f6e818d4b2a850e5c9
Katie McCauley <katiemccau02@gmail.com>
2022-04-16 03:05:13 -0600

    profile page loooks better

--- keeper.client/src/pages/ProfilePage.vue
+++ keeper.client/src/pages/ProfilePage.vue
@@ -1,11 +1,32 @@
 <template>
   <div class="container-fluid mt-2">
-    <img :src="profile.picture" class="img-fluid cropped" alt="" />
-    <h1>{{ profile.name }}</h1>
+    <div class="row m-3 ms-0">
+      <div class="col-1">
+        <img :src="profile.picture" class="img-fluid cropped" alt="" />
+      </div>
+      <div class="col-5">
+        <h1>{{ profile.name }}</h1>
+        <h3>Keeps: {{ keeps.length }}</h3>
+        <h3>Vaults: {{ vaults.length }}</h3>
+      </div>
+    </div>
+    <h2>Keeps:</h2>
     <div class="masonry-with-columns">
       <div class="" v-for="k in keeps" :key="k.id">
         <!-- <img :src="k.img" class="rounded cropped img-fluid" alt="" /> -->
         <Keep :keepData="k" />
+      </div>
+    </div>
+    <h2>Vaults:</h2>
+    <div class="masonry-with-columns">
+      <div v-for="v in vaults" :key="v.id">
+        <div class="card selectable">
+          <img :src="v.img" class="img-fluid" alt="" />
+          <div class="card-img-overlay">
+            <h1>{{ v.name }}</h1>
+            <img :src="v.creator.picture" class="img-fluid cropped" alt="" />
+          </div>
+        </div>
       </div>
     </div>
   </div>
